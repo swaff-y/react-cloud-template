6c3220ba016967bd572805de6204bb6f157180f1
swaff-y <kyle@swaff.id.au>
2023-05-30 23:44:36 +1000

    RCT-1 Update build script

--- .buildkite/scripts/build_and_test.sh
+++ .buildkite/scripts/build_and_test.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-docker build -t react-cloud-template:dev .
-
-docker run --rm react-cloud-template:dev npm test &> coverage
-
-# if [ "$BUILDKITE_BRANCH" == "main"  ]
-# then
-
-# else
-
-# fi
+if [ "$BUILDKITE_BRANCH" == "main"  ]
+then
+  docker build -t react-cloud-template:prod .
+  docker run --rm react-cloud-template:prod npm test &> coverage
+else
+  docker build -t react-cloud-template:dev .
+  docker run --rm react-cloud-template:dev npm test &> coverage
+fi
