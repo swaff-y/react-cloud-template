a4042a90bafe1ed4050bbf6ad10d5fb26dfe286b
swaff-y <kyle@swaff.id.au>
2023-06-01 23:32:45 +1000

    RCT-1 Update build script

--- .buildkite/scripts/parse_results.rb
+++ .buildkite/scripts/parse_results.rb
@@ -5,9 +5,14 @@ require 'json'
 #read file contents
 if ARGV[0]
     file = File.read("./#{ARGV[0]}")
-    data_hash = JSON.parse(file)
-    puts data_hash
+    check_results(JSON.parse(file))
 else
-    puts "A data file is required"
-    return
+    puts 'A data file is required'
+end
+
+def check_results(data)
+  failed_tests = data['numFailedTests']
+  return puts "\33[32;37mTests Passed\33[0m" unless data['numFailedTests'] <= 0
+
+  puts "Number of failed tests: #{failed_tests}"
 end
