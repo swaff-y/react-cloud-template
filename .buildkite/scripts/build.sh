241622dc4b05a4cab81c7e91b71a921109288152
swaff-y <kyle@swaff.id.au>
2023-06-05 22:24:49 +1000

    RCT-1 Update deploy

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -7,15 +7,15 @@ $APPLICATION
 
 if [ "$BUILDKITE_BRANCH" == "main"  ]
 then
+  aws ssm get-parameter --name "react-cloud-temp-prod" | jq .Parameter.Value | jq -r . > prod.env
   export STAGE=prod
   export APPLICATION=$(cat prod.env | grep APPLICATION | sed "s/^.*='\(.*\)'$/\1/")
-  aws ssm get-parameter --name "react-cloud-temp-prod" | jq .Parameter.Value | jq -r . > prod.env
   npm run build:prod
   ruby .buildkite/scripts/create_stack.rb serverless.yml
 else
+  aws ssm get-parameter --name "react-cloud-temp-dev" | jq .Parameter.Value | jq -r . > dev.env
   export STAGE=dev
   export APPLICATION=$(cat dev.env | grep APPLICATION | sed "s/^.*='\(.*\)'$/\1/")
-  aws ssm get-parameter --name "react-cloud-temp-dev" | jq .Parameter.Value | jq -r . > dev.env
   npm run build:dev
   ruby .buildkite/scripts/create_stack.rb serverless.yml
 fi
