9d443b2a3214e10cf982c13ddb256e548b4475db
swaff-y <kyle@swaff.id.au>
2023-06-05 21:56:04 +1000

    RCT-1 Update deploy

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -3,13 +3,21 @@
 set -e
 
 npm install
+$APPLICATION
 
 if [ "$BUILDKITE_BRANCH" == "main"  ]
 then
+  export STAGE=prod
+  export APPLICATION=$(cat prod.env | grep APPLICATION | sed "s/^.*='\(.*\)'$/\1/")
+  aws ssm get-parameter --name "react-cloud-temp-prod" | jq .Parameter.Value | jq -r . > prod.env
   npm run build:prod
+  ruby .buildkite/scripts/create_stack.rb serverless.yml
 else
+  export STAGE=dev
+  export APPLICATION=$(cat dev.env | grep APPLICATION | sed "s/^.*='\(.*\)'$/\1/")
   aws ssm get-parameter --name "react-cloud-temp-dev" | jq .Parameter.Value | jq -r . > dev.env
   npm run build:dev
+  ruby .buildkite/scripts/create_stack.rb serverless.yml
 fi
 
-zip -r build.zip build
+zip -r build.zip build serverless.yml
--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 buildkite-agent artifact download build.zip .
+rm serverless.yml
 unzip build.zip -d .
 npm install
 sls remove
